8a3736d3ff510a40ffb0a3ae5605b3ff10903e68
Richards <CNR1103@daugherty.com>
2020-09-25 19:53:58 -0500

    Initial structure setup and added css, js, and html source files for selenium testing

---
+++ dompiggame/src/main/java/com/chaserichards/dompiggame/DompiggameApplication.java
@@ -0,0 +1,13 @@
+package com.chaserichards.dompiggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DompiggameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DompiggameApplication.class, args);
+	}
+
+}
---
+++ dompiggame/src/test/java/com/chaserichards/dompiggame/DompiggameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaserichards.dompiggame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DompiggameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
