043c39b2ff21945bbe2113cd4c8f64ea7fa7b5cb
Richards <CNR1103@daugherty.com>
2020-09-27 10:52:38 -0500

    Added test to confirm we make it to the results page after going through 26 questions

--- myprioritiesquiz/src/test/java/com/chaserichards/myprioritiesquiz/MyPrioritiesTests.java
+++ myprioritiesquiz/src/test/java/com/chaserichards/myprioritiesquiz/MyPrioritiesTests.java
@@ -6,18 +6,23 @@ import com.chaserichards.myprioritiesquiz.webdrivers.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class MyPrioritiesTests {
     protected WebDriver driver;
+    private MyPrioritiesHomePage homePage;
+    private MyPrioritiesQuiz quizPage;
 
     @BeforeTest
     public void localSetup() {
+        //Arrange
         //Use driver factory to setup a new chrome driver
         driver = DriverFactory.getWebDriver("chrome");
-
+        homePage = new MyPrioritiesHomePage(driver);
+        quiz
     }
 
     @AfterTest
@@ -25,26 +30,47 @@ public class MyPrioritiesTests {
         driver.quit();
     }
 
-    @Test
+    @Test(description = "Test to open the homepage and begin the quiz")
     void openHomepageAndBeginQuiz() throws InterruptedException {
+        //Arrange
+        var expectedURl = "https://mypriorities.edwardjones.com/quiz";
 
-        var home = new MyPrioritiesHomePage(driver);
-        home
+        //Act
+        homePage
                 .navigateToMyPriorHomePage()
                 .beginQuiz();
 
+        //Assert
+        Assert.assertEquals(driver.getCurrentUrl(), expectedURl);
+
         Thread.sleep(3000);
     }
 
-    @Test
-    void chooseOptionCardOne() throws InterruptedException {
-        var quizPage = new MyPrioritiesQuiz(driver);
+    @Test(description = "Test to confirm that a random card (1 or 2) can be selected through the entire quiz and makes it to results page")
+    void chooseOptionsUntilResultsPage() throws InterruptedException {
+        //Arrange
+        MyPrioritiesQuiz quizPage = new MyPrioritiesQuiz(driver);
+        var expectedURL = "https://mypriorities.edwardjones.com/results";
+
+        //Act
         openHomepageAndBeginQuiz();
-        quizPage.chooseAnswer();
+        for(var i = 0; i < 26; i++) {
+            quizPage.chooseAnswer();
+        }
+
+        //Wait for the page the results page to load
+        quizPage.resultsPage();
+
+        //Assert
+        Assert.assertEquals(driver.getCurrentUrl(), expectedURL);
 
         Thread.sleep(3000);
     }
 
+    @Test(description = "Test to confirm change previous answer button works")
+    void changePreviousAnswerButton() throws InterruptedException {
+        //Arrange
+    }
 
 }
 
--- myprioritiesquiz/src/test/java/com/chaserichards/myprioritiesquiz/pages/mypriorities/MyPrioritiesQuiz.java
+++ myprioritiesquiz/src/test/java/com/chaserichards/myprioritiesquiz/pages/mypriorities/MyPrioritiesQuiz.java
@@ -3,6 +3,7 @@ package com.chaserichards.myprioritiesquiz.pages.mypriorities;
 import com.chaserichards.myprioritiesquiz.pages.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class MyPrioritiesQuiz extends BasePage {
@@ -10,7 +11,15 @@ public class MyPrioritiesQuiz extends BasePage {
     public MyPrioritiesQuiz(WebDriver webDriver) { super(webDriver); }
 
     public MyPrioritiesQuiz chooseAnswer() {
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("value-card-one"))).click();
+        //Generate random value of either 1 or 2
+        var randomAnswer = (Math.random() <= 0.5) ? "one" : "two";
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("value-card-" + randomAnswer))).click();
         return this;
+    }
+
+    //Return void because we change to the results page
+    public void resultsPage() {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("ResultsPage")));
     }
 }
