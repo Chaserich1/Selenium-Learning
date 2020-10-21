7149064eff71d8983d07b85d630072e17cdcda7c
Richards <CNR1103@daugherty.com>
2020-10-21 13:35:11 -0500

    Updated 401 test to use a dataprovider rather than three seperate tests

--- WebApiAutomation/src/test/java/Get401.java
+++ WebApiAutomation/src/test/java/Get401.java
@@ -5,6 +5,7 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -27,34 +28,19 @@ public class Get401 extends BaseTestClass{
         response.close();
     }
 
-    @Test
-    public void userReturns401() throws IOException {
-
-        HttpGet get = new HttpGet(BASE_ENDPOINT + "/user");
-
-        response = client.execute(get);
-
-        int actualStatus = response.getStatusLine().getStatusCode();
-
-        assertEquals(actualStatus, 401);
+    @DataProvider
+    private Object[][] endpoints() {
+        return new Object[][] {
+                {"/user"},
+                {"/user/followers"},
+                {"/notifications"}
+        };
     }
 
-    @Test
-    public void userFollowersReturns401() throws IOException {
+    @Test(dataProvider = "endpoints")
+    public void userReturns401(String endpoint) throws IOException {
 
-        HttpGet get = new HttpGet(BASE_ENDPOINT + "/user/followers");
-
-        response = client.execute(get);
-
-        int actualStatus = response.getStatusLine().getStatusCode();
-
-        assertEquals(actualStatus, 401);
-    }
-
-    @Test
-    public void notificationsReturns401() throws IOException {
-
-        HttpGet get = new HttpGet(BASE_ENDPOINT + "/notifications");
+        HttpGet get = new HttpGet(BASE_ENDPOINT + endpoint);
 
         response = client.execute(get);
 
